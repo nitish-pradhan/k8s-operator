c52d41b7d93444300629dd86c8205c6086c4c7df
Nitish Kumar Pradhan <nitishkumarpradhan@fico.com>
2023-08-20 12:08:36 +0530

    Fixed issues with virtual env

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,24 +1,17 @@
 from fastapi import APIRouter, HTTPException
 
-from models.user import User
-from config.db import collection
-from schemas.user import serializeDict, serializeList
+from app.models.user import User
+from app.config.db import collection
+from app.schemas.user import serializeDict, serializeList
 from bson import ObjectId
 user = APIRouter()
 
-# @user.get('/')
-# async def find_all_users():
-#     return serializeList(conn.local.user.find())
 
 @user.get("/users/", response_model=list[User])
 async def find_all_users(skip: int = 0, limit: int = 10):
     users = collection.find().skip(skip).limit(limit)
     #return list(users)
     return serializeList(users)
-#
-# @user.get('/{id}')
-# async def find_one_user(id):
-#     return serializeDict(conn.local.user.find_one({"_id":ObjectId(id)}))
 
 @user.get("/users/{id}", response_model=User)
 async def find_one_user(id: str):
@@ -27,20 +20,10 @@ async def find_one_user(id: str):
         raise HTTPException(status_code=404, detail="User not found")
     return user
 
-# @user.post('/')
-# async def create_user(user: User):
-#     conn.local.user.insert_one(dict(user))
-#     return serializeList(conn.local.user.find())
-
 @user.post("/users/", response_model=User)
 async def create_user(user: User):
     inserted_item = collection.insert_one(dict(user))
     return {"id": str(inserted_item.inserted_id), **user.dict()}
-
-# @user.put('/{id}')
-# async def update_user(id, user: User):
-#     conn.local.user.find_one_and_update({"_id":ObjectId(id)}, {"$set": dict(user)})
-#     return serializeDict(conn.local.user.find_one({"_id":ObjectId(id)}))
 
 @user.put("/users/{id}", response_model=User)
 async def update_user(id: str, user: User):
@@ -50,10 +33,6 @@ async def update_user(id: str, user: User):
 
     collection.find_one_and_update({"_id":ObjectId(id)}, {"$set": dict(user)})
     return serializeDict(collection.find_one({"_id":ObjectId(id)}))
-#
-# @user.delete('/{id}')
-# async def delete_user(id, user: User):
-#     return serializeDict(conn.local.user.find_one_and_delete({"_id":ObjectId(id)}))
 
 @user.delete("/users/{id}", response_model=dict)
 async def delete_user(id: str):
