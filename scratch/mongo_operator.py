6f93f7dd638322ef07c5dd1d01ad58df74a9e5ea
Nitish Kumar Pradhan <nitishkumarpradhan@fico.com>
2023-08-21 17:27:38 +0530

    Added operator files

--- scratch/mongo_operator.py
+++ scratch/mongo_operator.py
@@ -1,27 +1,17 @@
 import kopf
 import os
 from kubernetes import config
-from typing import Any, Dict, List, Optional, Tuple, cast
-import asyncio
-from kubernetes_asyncio.client import (
-    AppsV1Api,
-    CoreV1Api,
-    CustomObjectsApi,
-    V1Namespace,
-    V1PersistentVolumeClaimList,
-    V1PodList,
-    V1Service,
-    V1ServiceList,
-    V1StatefulSet,
-    V1StatefulSetList,
-)
-from kubernetes_asyncio.client.api_client import ApiClient
 
 def initialize_kube():
-    print("Loading from local kube config")
-    home = os.path.expanduser("~")
-    kube_config_path = os.getenv("KUBE_CONFIG", home + "/.kube/config")
-    config.load_kube_config(config_file=kube_config_path)
+    DEV = os.getenv('DEV')
+    if DEV:
+        print ("Loading from local kube config")
+        home = os.path.expanduser("~")
+        kube_config_path = os.getenv("KUBE_CONFIG", home+"/.kube/config")
+        config.load_kube_config(config_file=kube_config_path)
+    else:
+        print ("Loading In-cluster config")
+        config.load_incluster_config()
 
 @kopf.on.create('mongodbs')
 def create_fn(spec, **kwargs):
@@ -29,21 +19,4 @@ def create_fn(spec, **kwargs):
     return {'message': 'hello world'}  # will be the new status
 
 
-async def get_pvcs_in_namespace(
-    core: CoreV1Api, namespace: str, name: str) -> List[Dict[str, str]]:
-    labels = {
-        LABEL_NAME: name
-    }
-    label_selector = ",".join(f"{k}={v}" for k, v in labels.items())
-
-    all_pvcs: V1PersistentVolumeClaimList = (
-        await core.list_namespaced_persistent_volume_claim(
-            namespace=namespace, label_selector=label_selector
-        )
-    )
-    result = [{"uid": p.metadata.uid, "name": p.metadata.name} for p in all_pvcs.items]
-    print(result)
-    return [{"uid": p.metadata.uid, "name": p.metadata.name} for p in all_pvcs.items]
-
 initialize_kube()
-get_pvcs_in_namespace()
