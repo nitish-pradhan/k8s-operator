e2c6006b633ad599563f56951bb91d5622a33e77
Nitish Kumar Pradhan <nitishkumarpradhan@fico.com>
2023-08-23 17:11:06 +0530

    Added authentication

---
+++ app/auth/deps.py
@@ -0,0 +1,53 @@
+from datetime import datetime
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from .utils import (
+    ALGORITHM,
+    JWT_SECRET_KEY
+)
+
+from jose import jwt
+from pydantic import ValidationError
+from .schemas import TokenPayload
+from schemas.user import serializeDict, serializeList
+from config.db import collection
+import  logging
+logging.getLogger().setLevel(logging.INFO)
+logging.basicConfig(level=logging.INFO,
+                        format='%(levelname)s: %(asctime)s: %(message)s')
+
+reuseable_oauth = OAuth2PasswordBearer(
+    tokenUrl="/login",
+    scheme_name="JWT"
+)
+
+
+async def get_current_user(token: str = Depends(reuseable_oauth)):
+    try:
+        payload = jwt.decode(
+            token, JWT_SECRET_KEY, algorithms=[ALGORITHM]
+        )
+        token_data = TokenPayload(**payload)
+
+        if datetime.fromtimestamp(token_data.exp) < datetime.now():
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Token expired",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+    except(jwt.JWTError, ValidationError):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    user = collection.find_one({"email": token_data.sub})
+
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Could not find user",
+        )
+
+    return user
--- app/routes/user.py
+++ app/routes/user.py
@@ -1,31 +1,53 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, Depends
 
-from app.models.user import User
-from app.config.db import collection
-from app.schemas.user import serializeDict, serializeList
+from models.user import User
+from config.db import collection
+from schemas.user import serializeDict, serializeList
 from bson import ObjectId
+
+from fastapi import status, HTTPException, Depends
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.responses import RedirectResponse
+from auth.schemas import UserAuth, TokenSchema
+from auth.utils import (
+    get_hashed_password,
+    create_access_token,
+    create_refresh_token,
+    verify_password
+)
+from auth.deps import get_current_user
+import logging
+
+logging.getLogger().setLevel(logging.INFO)
+logging.basicConfig(level=logging.INFO,
+                        format='%(levelname)s: %(asctime)s: %(message)s')
+
 user = APIRouter()
 
 
-@user.get("/users/", response_model=list[User])
+@user.get('/me', summary='Get details of currently logged in user', response_model=dict)
+async def get_me(user: User = Depends(get_current_user)):
+    return {'name': user['name'], 'email': user['email']}
+
+
+@user.get("/users/", response_model=list[User], dependencies=[Depends(get_current_user)])
 async def find_all_users(skip: int = 0, limit: int = 10):
     users = collection.find().skip(skip).limit(limit)
-    #return list(users)
     return serializeList(users)
 
-@user.get("/users/{id}", response_model=User)
-async def find_one_user(id: str):
-    user = collection.find_one({"_id":ObjectId(id)})
+@user.get("/users/{id}", response_model=User, dependencies=[Depends(get_current_user)])
+async def find_one_user(name: str):
+    user = collection.find_one({"name": name})
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return user
 
-@user.post("/users/", response_model=User)
+@user.post("/users/", response_model=User, dependencies=[Depends(get_current_user)])
 async def create_user(user: User):
     inserted_item = collection.insert_one(dict(user))
     return {"id": str(inserted_item.inserted_id), **user.dict()}
 
-@user.put("/users/{id}", response_model=User)
+@user.put("/users/{id}", response_model=User, dependencies=[Depends(get_current_user)])
 async def update_user(id: str, user: User):
     existing_user = collection.find_one({"_id":ObjectId(id)})
     if existing_user is None:
@@ -34,9 +56,49 @@ async def update_user(id: str, user: User):
     collection.find_one_and_update({"_id":ObjectId(id)}, {"$set": dict(user)})
     return serializeDict(collection.find_one({"_id":ObjectId(id)}))
 
-@user.delete("/users/{id}", response_model=dict)
+@user.delete("/users/{id}", response_model=dict, dependencies=[Depends(get_current_user)])
 async def delete_user(id: str):
     deletion_result = collection.delete_one({"_id": ObjectId(id)})
     if deletion_result.deleted_count == 0:
         raise HTTPException(status_code=404, detail="User not found")
     return {"status": "success", "message": "User deleted"}
+
+
+@user.get('/', response_class=RedirectResponse, include_in_schema=False)
+async def docs():
+    return RedirectResponse(url='/docs')
+
+@user.post('/signup', summary="Create new user", response_model=User, dependencies=[Depends(get_current_user)])
+async def create_user(user: User):
+    # querying database to check if user already exist
+    check = collection.find_one({"email": user.email})
+    if check is not None:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User with this email already exist"
+        )
+
+    user.password = get_hashed_password(user.password)
+    inserted_item = collection.insert_one(dict(user))
+    return {"id": str(inserted_item.inserted_id), **user.dict()}
+
+
+@user.post('/login', summary="Create access and refresh tokens for user", response_model=TokenSchema)
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = collection.find_one({"name": form_data.username})
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Incorrect email or password"
+        )
+    hashed_password = user['password']
+    if not verify_password(form_data.password, hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Incorrect email or password"
+        )
+
+    return {
+        "access_token": create_access_token(user['email']),
+        "refresh_token": create_refresh_token(user['email']),
+    }
